bab4a49a2d1bed7eaf06d4c8c6720fd8f7028df5
fili0x232f <30128845+fili0x232f@users.noreply.github.com>
2021-05-27 21:38:11 +0300

    base version

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -22,6 +25,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String millisecondsText = "";
+  GameState gameState = GameState.readyToStart;
+
+  Timer? waitingTimer;
+  Timer? stoppableTimer;
 
   @override
   Widget build(BuildContext context) {
@@ -59,15 +66,29 @@ class _MyHomePageState extends State<MyHomePage> {
           Align(
             alignment: const Alignment(0, 0.9),
             child: GestureDetector(
-              onTap: () => setState(() => millisecondsText = "149 ms"),
+              onTap: () => setState(() {
+                switch (gameState) {
+                  case GameState.readyToStart:
+                    gameState = GameState.waiting;
+                    millisecondsText = "";
+                    _startWaitingTimer();
+                    break;
+                  case GameState.waiting:
+                    gameState = GameState.canBeStopped;
+                    break;
+                  case GameState.canBeStopped:
+                    gameState = GameState.readyToStart;
+                    stoppableTimer?.cancel();
+                    break;
+                }
+              }),
               child: ColoredBox(
                 color: Colors.black12,
                 child: SizedBox(
                   height: 200,
                   width: 200,
                   child: Center(
-                    child: Text("START",
-                        textAlign: TextAlign.center,
+                    child: Text(_getButtonText(),
                         style: TextStyle(
                             fontSize: 35,
                             fontWeight: FontWeight.w700,
@@ -80,6 +101,42 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
     );
+  }
+
+  String _getButtonText() {
+    switch (gameState) {
+      case GameState.readyToStart:
+        return "START";
+      case GameState.waiting:
+        return "WAIT";
+      case GameState.canBeStopped:
+        return "STOP";
+    }
+  }
+
+  void _startWaitingTimer() {
+    final int randomMilliseconds = Random().nextInt(4000) + 1000;
+    waitingTimer = Timer(Duration(milliseconds: randomMilliseconds), () {
+      setState(() {
+        gameState = GameState.canBeStopped;
+      });
+      _startStoppableTimer();
+    });
+  }
+
+  void _startStoppableTimer() {
+    stoppableTimer = Timer.periodic(Duration(milliseconds: 16), (timer) {
+      setState(() {
+        millisecondsText = "${timer.tick * 16} ms";
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    waitingTimer?.cancel();
+    stoppableTimer?.cancel();
+    super.dispose();
   }
 }
 
